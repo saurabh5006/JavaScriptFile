4dc33e96ad441b7fdd0df8c094c7328651a80a68
Powerhf <itsupport3@powerhf>
2025-03-27 12:32:52 +0530

    added a code and the new file of the Return.js

--- practice.js
+++ practice.js
@@ -1,3 +1,15 @@
+function multiple_value(fun,count){
+        for(let i = 1; i<=10; i++){
+            fun();
+        }
+}
+
 let greet = function(){
 console.log("hello saurabh");
 };
+
+multiple_value(greet, 10);
+
+
+
+
